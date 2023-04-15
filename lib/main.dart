2d702e758f4455367186bf4eb377b6f71a85fb4c
권용환 <hahahaa_a@naver.com>
2023-04-15 19:27:08 +0900

    배치 관련 위젯 - Row

--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,41 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(RowWidgetExample());
 }
 
-class MyApp extends StatelessWidget {
+class RowWidgetExample extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        body: Center(
-          child: SafeArea(
-            top: true,
-            bottom: true,
-            left: true,
-            right: true,
-            child: Container(
-              color: Colors.red,
-              height: 300,
-              width: 300,
-            ),
+        body: SizedBox(
+          height: double.infinity,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.center,
+
+            children: [
+              Container(
+                height: 50,
+                width: 50,
+                color: Colors.red,
+              ),
+
+              const SizedBox(width: 12,),
+              Container(
+                height: 50,
+                width: 50,
+                color: Colors.green,
+              ),
+              const SizedBox(width: 12,),
+              Container(
+                height: 50,
+                width: 50,
+                color: Colors.blue,
+              )
+            ],
           ),
         ),
       ),
