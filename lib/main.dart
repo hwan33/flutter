f38f9d4d4a4fc90ad1a49c6e889480e603800d6f
권용환 <hahahaa_a@naver.com>
2023-04-15 14:34:21 +0900

    디자인 관련 위젯 - margin

--- lib/main.dart
+++ lib/main.dart
@@ -12,13 +12,17 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         body: Center(
           child: Container(
-            color: Colors.blue,
-            child: Padding(
-              padding: EdgeInsets.all(16.0),
-              child: Container(
-                color: Colors.red,
-                width: 50.0,
-                height: 50.0,
+            color: Colors.black,
+            child: Container(
+              color: Colors.blue,
+              margin: EdgeInsets.all(16.0),
+              child: Padding(
+                padding: EdgeInsets.all(16.0),
+                child: Container(
+                  color: Colors.red,
+                  height: 50,
+                  width: 50,
+                ),
               ),
             ),
           ),
