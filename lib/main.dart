84458217700c6c423ce5835379ba7dd165419037
권용환 <hahahaa_a@naver.com>
2023-04-15 14:34:08 +0900

    디자인 관련 위젯 - Padding

--- lib/main.dart
+++ lib/main.dart
@@ -11,12 +11,15 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         body: Center(
-          child: SizedBox(
-            height: 200.0,
-            width: 200.0,
-
-            child: Container(
-              color: Colors.red,
+          child: Container(
+            color: Colors.blue,
+            child: Padding(
+              padding: EdgeInsets.all(16.0),
+              child: Container(
+                color: Colors.red,
+                width: 50.0,
+                height: 50.0,
+              ),
             ),
           ),
         ),
